02fc146ac558de1a3c2e2d9ce4e7e0f57d13b1d1
choZopu <116959654+choZopu@users.noreply.github.com>
2022-10-29 20:42:23 +0700

    Create Key.lua

---
+++ Key.lua
@@ -0,0 +1,6 @@
+if _G.Key == 1234 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/4429189426bd1d66d97857e45935b59a7bc20b32/README.lua"))()
+end
+if _G.Key ~= 1234 then 
+ print("nokey")
+end
