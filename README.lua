0d424126c8821da93da72b33f9e3ac23fb6c14a0
choZopu <116959654+choZopu@users.noreply.github.com>
2022-10-30 18:53:59 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -47,4 +47,43 @@ Section:NewButton("killer esp", "", function()
 		CreateESPPart(workspace.Killer.RightUpperLeg, Color3.fromRGB(255, 0, 4))
 		CreateESPPart(workspace.Killer.LowerTorso, Color3.fromRGB(255, 0, 4))
 		CreateESPPart(workspace.Killer.Head, Color3.fromRGB(255, 0, 4))
-	end)
+end)
+
+Section:NewToggle("ESPPLayer", "ToggleInfo", function(state)
+    _G.espplayer = state
+	while _G.espplayer do wait(0.1)
+     pcall(function()
+       for i,v in pairs(game.Players:GetChildren()) do
+            if not v.Character.Head:FindFirstChild("ESP") then
+                local BillboardGui = Instance.new("BillboardGui")
+                local TextLabel = Instance.new("TextLabel")
+                BillboardGui.Parent = v.Character.Head
+                BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+                BillboardGui.Active = true
+                BillboardGui.Name = "ESP"
+                BillboardGui.AlwaysOnTop = true
+                BillboardGui.LightInfluence = 1.000
+                BillboardGui.Size = UDim2.new(0, 200, 0, 50)
+                BillboardGui.StudsOffset = Vector3.new(0, 2.5, 0)
+                TextLabel.Parent = BillboardGui
+                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+                TextLabel.BackgroundTransparency = 1.000
+                TextLabel.Size = UDim2.new(0, 100, 0, 50)
+                TextLabel.Font = Enum.Font.GothamBold
+                TextLabel.Text = v.Name
+                TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+                TextLabel.TextScaled = true
+                TextLabel.TextSize = 9.000
+                TextLabel.TextStrokeTransparency = 0.000
+                TextLabel.TextWrapped = true
+            end
+        end
+    end) 
+end
+end)
+
+
+
+
+
+
