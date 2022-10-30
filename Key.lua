a9b360b3281ea0b0900b77b000c4b3615aa8f12a
choZopu <116959654+choZopu@users.noreply.github.com>
2022-10-30 18:55:45 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,5 +1,5 @@
 if _G.Key == 1234 then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/4429189426bd1d66d97857e45935b59a7bc20b32/README.lua"))()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/main/README.lua"))()
 end
 if _G.Key ~= 1234 then 
  print("nokey")
