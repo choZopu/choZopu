90d0a94959650c83ef54b8755b924dff015e4fe2
choZopu <116959654+choZopu@users.noreply.github.com>
2022-10-31 21:00:24 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,4 +1,4 @@
-if _G.Key == 1234 then
+if _G.Key == 1234 and _G.IDPASS == POLLLEE X POTTER then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/main/README.lua"))()
 end
 if _G.Key ~= 1234 then 
