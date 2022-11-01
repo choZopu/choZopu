8e43c06b85e39d2dae7cb6acaadfee436b9ffbb9
choZopu <116959654+choZopu@users.noreply.github.com>
2022-11-01 08:14:03 +0700

    Update and rename Key.lua to POLLEEHUB.lua

--- Key.lua
+++
@@ -1,6 +0,0 @@
-if _G.Key == 1234 and _G.IDPASS == "POLLLEE X POTTER" then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/main/README.lua"))()
-end
-if _G.Key ~= 1234 then 
- print("nokey")
-end
---
+++ POLLEEHUB.lua
@@ -0,0 +1,7 @@
+if _G.Key == 1234 then 
+ if _G.IDPASS == POLLEEHUB then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/main/README.lua"))()
+end
+if _G.Key ~= 1234 then 
+ print("nokey")
+end
