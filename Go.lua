d9c97ef74e436fd376d876afa9266cbd58b58be8
choZopu <116959654+choZopu@users.noreply.github.com>
2023-02-05 15:39:04 +0700

    Update Go.lua

--- Go.lua
+++ Go.lua
@@ -2,6 +2,12 @@ if game:GetService("CoreGui"):FindFirstChild("PULYHUB") then
     game:GetService("CoreGui"):FindFirstChild("PULYHUB"):Destroy()
 end
 
+repeat wait()
+until game:IsLoaded()
+wait(1)
+repeat wait()
+until game.Players.LocalPlayer.Character.HumanoidRootPart
+
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/choZopu/choZopu/main/Gui"))()
 local venyx = library.new("PULYHUB")
 local page = venyx:addPage("players")
@@ -24,13 +30,17 @@ for i,v in pairs(game:GetService("Players"):GetChildren()) do
    table.insert(players,v.Name)
 end
 end)
-
+Speed = 150
+section1:addSlider("speed", 0, 0, 200, function(spe)
+    Speed = spe
+    return Speed
+end)
 section1:addSlider("X", 0, -20, 20, function(x)
     X = x
     return X
 end)
 
-section1:addSlider("Y", 5, -20, 20, function(y)
+section1:addSlider("Y", 0, -20, 20, function(y)
     Y = y
     return Y
 end)
@@ -45,17 +55,24 @@ section1:addToggle("GO TO Player", false, function(t)
 end) 
 
 spawn(function()
- while wait(0) do
+ while wait(0.1) do
 	 if _G.TPTPPL then
 	     repeat wait()
 		 Distance = (game.Players[Select].Character.HumanoidRootPart.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude 
-         Speed = 150
 		 dri = math.floor(Distance/Speed) + 0.1
          tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(dri, Enum.EasingStyle.Linear)
          tween = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = game.Players[Select].Character.HumanoidRootPart.CFrame * CFrame.new(X,Y,Z)})
          tween:Play()
-	     until _G.TPTPPL == false 
+	     until _G.TPTPPL == false
 	 end
+	 if game.Players.LocalPlayer.Character.Humanoid.Health <= 0 then
+        _G.TPTPPL = false
+        wait(3)
+        _G.TPTPPL = true
+    end
+     if _G.TPTPPL then
+         tween:Cancel()
+     end
  end
 end)
 
@@ -66,11 +83,12 @@ spawn(function()
             setfflag("HumanoidParallelRemoveNoPhysics", "False")
             setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
             game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(11)
-            end
         end
+    end
     end)
 end)
 section1:addKeybind("Toggle Keybind", Enum.KeyCode.RightControl, function()
 	venyx:toggle()
 end)
 venyx:SelectPage(venyx.pages[1], true)
+
