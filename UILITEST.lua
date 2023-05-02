0f6236dfe377e4b4ca2faf2b2bf0bfa70ff0dd7e
choZopu <116959654+choZopu@users.noreply.github.com>
2023-05-02 20:33:07 +0700

    Update UILITEST.lua

--- UILITEST.lua
+++ UILITEST.lua
@@ -2948,7 +2948,7 @@ function Library:CreateWindow(...)
     if type(Config.MenuFadeTime) ~= 'number' then Config.MenuFadeTime = 0.2 end
 
     if typeof(Config.Position) ~= 'UDim2' then Config.Position = UDim2.fromOffset(175, 50) end
-    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(550, 600) end
+    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(200, 300) end
 
     if Config.Center then
         Config.AnchorPoint = Vector2.new(0.5, 0.5)
